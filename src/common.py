ba2fdff047236ceaebf142c7535b1881d8a3a957
Tim Reynolds <timbreynolds@gmail.com>
2017-10-11 09:13:08 -0400

    Fix active detection for dropdowns by moving it to python.

--- src/common.py
+++ src/common.py
@@ -7,3 +7,9 @@ class Common(object):
 
   def uri_matches(self, uri):
     return self.uri.endswith(uri)
+
+  def uri_matches_any(self, uri_list):
+    for text, uri in uri_list:
+      if self.uri_matches(uri):
+        return True
+    return False
