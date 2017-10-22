b5189e9300572a20855c1211e2f65a991d63a86a
Tim Reynolds <timbreynolds@gmail.com>
2017-10-22 19:38:18 -0400

    Add fields to Region and State.

--- src/models/region.py
+++ src/models/region.py
@@ -2,3 +2,4 @@ from google.appengine.ext import ndb
 
 class Region(ndb.Model):
   name = ndb.StringProperty()
+  championship_name = ndb.StringProperty()
--- src/models/state.py
+++ src/models/state.py
@@ -5,3 +5,4 @@ from src.models.region import Region
 class State(ndb.Model):
   name = ndb.StringProperty()
   region = ndb.KeyProperty(kind=Region)
+  is_state = ndb.BooleanProperty()
