0dee5d2f3d3f3d8f6cb4c341702b65030d9767ab
Tim Reynolds <timbreynolds@gmail.com>
2017-10-28 13:44:46 -0400

    include the base model.

---
+++ src/models/wca/base.py
@@ -0,0 +1,9 @@
+from google.appengine.ext import ndb
+
+class BaseModel(ndb.Model):
+  @staticmethod
+  def GetId(row):
+    return row['id']
+
+  def ParseFromDict(self, row):
+    raise Exception('ParseFromDict is unimplemented.')
