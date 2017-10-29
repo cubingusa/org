8ed586360c64d5d0f9ea20a86cc875e61b69d734
Tim Reynolds <timbreynolds@gmail.com>
2017-10-29 12:01:36 -0400

    Change class name that's too similar to another.

--- src/static/competitions_us.js
+++ src/static/competitions_us.js
@@ -1,5 +1,5 @@
 selectCompEvent = function(event_id, event_name) {
-  var elts = document.getElementsByClassName('competition-row');
+  var elts = document.getElementsByClassName('competition-row-container');
   for (var i = 0; i < elts.length; i++) {
     evts = elts[i].dataset.evts.split(',');
     if (evts.includes(event_id)) {
