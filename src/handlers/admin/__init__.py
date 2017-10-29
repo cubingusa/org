01d506d392bf599eb51cec9b02833863b585fc0d
Tim Reynolds <timbreynolds@gmail.com>
2017-10-29 14:11:52 -0400

    Add a model for app settings, and a handler to update them.

--- src/handlers/admin/__init__.py
+++ src/handlers/admin/__init__.py
@@ -1,4 +1,5 @@
 from src.handlers.admin.get_wca_export import GetExportHandler
 from src.handlers.admin.post_import_mutations import PostImportMutationsHandler
+from src.handlers.admin.set_app_settings import SetAppSettingsHandler
 from src.handlers.admin.update_championships import UpdateChampionshipsHandler
 from src.handlers.admin.update_states import UpdateStatesHandler
---
+++ src/handlers/admin/set_app_settings.py
@@ -0,0 +1,19 @@
+import webapp2
+
+from src.models.app_settings import AppSettings
+
+class SetAppSettingsHandler(webapp2.RequestHandler):
+  def get(self, setting, value):
+    app_settings = AppSettings.Get()
+    if setting == 'session_secret_key':
+      app_settings.session_secret_key = value
+    elif setting == 'wca_oauth_client_id':
+      app_settings.wca_oauth_client_id = value
+    elif setting == 'wca_oauth_client_secret':
+      app_settings.wca_oauth_client_secret = value
+    else:
+      self.response.write('error: unrecognized setting %s' % setting)
+      self.response.set_status(400)
+      return
+    app_settings.put()
+    self.response.write('ok')
