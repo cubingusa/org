c97bb42ce7a8b7acc58399961bcf00635be325ec
Tim Reynolds <timbreynolds@gmail.com>
2017-10-29 11:18:58 -0400

    Stop logging the XMLHTTPRequest.

--- src/static/nationals.js
+++ src/static/nationals.js
@@ -2,7 +2,6 @@ setChampionsTable = function(event_id, event_name) {
   var req = new XMLHttpRequest();
   req.onreadystatechange = function() {
     if (req.readyState == 4) {
-      console.log(req);
       document.getElementById('nationals-evt').innerHTML = 'in ' + event_name;
       document.getElementById('champions-table').innerHTML = req.responseText;
     }
