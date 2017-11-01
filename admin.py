fb0ea88e95c11dd13aebc6af7b0ce8792d0f5ee8
Tim Reynolds <timbreynolds@gmail.com>
2017-11-01 08:57:02 -0400

    Change some URLs around in admin.
    Get rid of unnecessary /admin/, and add a /login handler.

--- admin.py
+++ admin.py
@@ -5,10 +5,16 @@ from src.handlers.admin.get_wca_export import GetExportHandler
 from src.handlers.admin.post_import_mutations import PostImportMutationsHandler
 from src.handlers.admin.update_championships import UpdateChampionshipsHandler
 from src.handlers.admin.update_states import UpdateStatesHandler
+from src.handlers.login import LoginHandler
+from src.handlers.login import LoginCallbackHandler
+from src.handlers.login import LogoutHandler
 
 app = webapp2.WSGIApplication([
-  webapp2.Route('/admin/post_import_mutations', handler=PostImportMutationsHandler),
-  webapp2.Route('/admin/update_championships', handler=UpdateChampionshipsHandler),
-  webapp2.Route('/admin/update_states', handler=UpdateStatesHandler),
-  webapp2.Route('/admin/wca/get_export', handler=GetExportHandler),
+  webapp2.Route('/login', handler=LoginHandler, name='login'),
+  webapp2.Route('/login_callback', handler=LoginCallbackHandler, name='login_callback'),
+  webapp2.Route('/logout', handler=LogoutHandler, name='logout'),
+  webapp2.Route('/post_import_mutations', handler=PostImportMutationsHandler),
+  webapp2.Route('/update_championships', handler=UpdateChampionshipsHandler),
+  webapp2.Route('/update_states', handler=UpdateStatesHandler),
+  webapp2.Route('/wca/get_export', handler=GetExportHandler),
 ], config=config.GetAppConfig())
--- admin_setup_app.py
+++ admin_setup_app.py
@@ -5,6 +5,6 @@ from src.handlers.admin.assign_role import AssignRoleHandler
 from src.handlers.admin.set_app_settings import SetAppSettingsHandler
 
 app = webapp2.WSGIApplication([
-  webapp2.Route('/admin/assign_role/<user_id:\d+>/<role:.*>', handler=AssignRoleHandler),
-  webapp2.Route('/admin/set_app_settings/<setting:.*>/<value:.*>', handler=SetAppSettingsHandler),
+  webapp2.Route('/setup/assign_role/<user_id:\d+>/<role:.*>', handler=AssignRoleHandler),
+  webapp2.Route('/setup/set_app_settings/<setting:.*>/<value:.*>', handler=SetAppSettingsHandler),
 ], config=config.GetAppConfig())
