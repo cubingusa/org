2e8964532cbe7c2add981feb5af74595a857473e
Tim Reynolds <timbreynolds@gmail.com>
2017-11-03 08:33:56 -0400

    Toggle admin in the document handler from config.

--- src/handlers/documents.py
+++ src/handlers/documents.py
@@ -10,40 +10,36 @@ from src.jinja import JINJA_ENVIRONMENT
 from src.models.document import Document
 
 
-def DocumentsHandler(is_admin):
-  class Handler(BaseHandler):
-    def get(self):
-      template = JINJA_ENVIRONMENT.get_template('documents.html')
-      documents_by_section = collections.defaultdict(list)
+class DocumentsHandler(BaseHandler):
+  def get(self):
+    is_admin = self.app.config.get('is_admin')
 
-      for document in Document.query().iter():
-        if not document.deletion_time:
-          documents_by_section[document.section].append(document)
+    template = JINJA_ENVIRONMENT.get_template('documents.html')
+    documents_by_section = collections.defaultdict(list)
 
-      template_dict = {
-          'c': common.Common(self),
-          'document_dict': documents_by_section,
-          'admin_page': is_admin,
-          'just_uploaded': self.request.get('uploaded') == '1',
-      }
+    for document in Document.query().iter():
+      if not document.deletion_time or is_admin:
+        documents_by_section[document.section].append(document)
 
-      if is_admin:
-        template_dict['upload_url'] = blobstore.create_upload_url(
-            webapp2.uri_for('upload_document'))
+    template_dict = {
+        'c': common.Common(self),
+        'documents_by_section': documents_by_section,
+        'admin_page': is_admin,
+        'just_uploaded': self.request.get('uploaded') == '1',
+    }
 
-      self.response.write(template.render(template_dict))
+    if is_admin:
+      template_dict['upload_url'] = blobstore.create_upload_url(
+          webapp2.uri_for('upload_document'))
 
-  return Handler
+    self.response.write(template.render(template_dict))
 
-def GetDocumentHandler(is_admin):
-  class Handler(blobstore_handlers.BlobstoreDownloadHandler):
-    def get(self, document_id, document_name):
-      document = Document.get_by_id(document_id)
-      if not document or not blobstore.get(document.blob_key):
-        self.error(404)
-      if not is_admin and document.deletion_time:
-        self.error(404)
-      else:
-        self.send_blob(document.blob_key)
-
-  return Handler
+class GetDocumentHandler(blobstore_handlers.BlobstoreDownloadHandler):
+  def get(self, document_id, document_name):
+    document = Document.get_by_id(document_id)
+    if not document or not blobstore.get(document.blob_key):
+      self.error(404)
+    if not self.app.config.get('is_admin') and document.deletion_time:
+      self.error(404)
+    else:
+      self.send_blob(document.blob_key)
