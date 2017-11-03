85e440aa29ca141d2a6913c09b99864afee7e33b
Tim Reynolds <timbreynolds@gmail.com>
2017-11-03 19:38:54 -0400

    Put is_admin in the right part of the config.

--- src/config.py
+++ src/config.py
@@ -1,10 +1,9 @@
 from src.models.app_settings import AppSettings
 
 def GetAppConfig(is_admin):
-  config = {}
-  config['webapp2_extras.sessions'] = {
-    'secret_key': str(AppSettings.Get().session_secret_key),
+  return {
     'is_admin': is_admin,
+    'webapp2_extras.sessions': {
+      'secret_key': str(AppSettings.Get().session_secret_key),
+    },
   }
-
-  return config
