a41d2a3f8b5de27d551a6110746fa52e0aa25a81
Tim Reynolds <timbreynolds@gmail.com>
2017-11-05 15:50:22 -0500

    Separate dev_app.sh from run_sass.sh.
    sass appears to have trouble when checking out a new branch.  Maybe reconsider this when all branches are using sass.

--- dev_app.sh
+++ dev_app.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-echo "Starting sass."
-sass --watch src/scss:src/static/css & sass_pid=$!
-
 echo "Starting dev_appserver.py."
-dev_appserver.py app.yaml admin.yaml & dev_appserver_pid=$!
-
-trap "kill -TERM $sass_pid $dev_appserver_pid" SIGINT
-
-wait $sass_pid $dev_appserver_pid
+dev_appserver.py app.yaml admin.yaml
