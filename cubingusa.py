bcfe809789385f04027b90c84aab41b94f9bed14
Tim Reynolds <timbreynolds@gmail.com>
2017-11-07 08:27:45 -0500

    Initial check-in of user listing.

--- cubingusa.py
+++ cubingusa.py
@@ -10,6 +10,7 @@ from src.handlers.documents import GetDocumentHandler
 from src.handlers.admin.documents import DeleteDocumentHandler
 from src.handlers.admin.documents import RestoreDocumentHandler
 from src.handlers.admin.documents import UploadDocumentHandler
+from src.handlers.admin.edit_users import EditUsersHandler
 from src.handlers.edit_user import EditUserHandler
 from src.handlers.login import LoginHandler
 from src.handlers.login import LoginCallbackHandler
@@ -21,7 +22,7 @@ app = webapp2.WSGIApplication([
   webapp2.Route('/login_callback', handler=LoginCallbackHandler, name='login_callback'),
   webapp2.Route('/logout', handler=LogoutHandler, name='logout'),
   webapp2.Route('/edit', handler=EditUserHandler, name='edit_user'),
-  webapp2.Route('/edit/<user_id:\d+>', handler=EditUserHandler, name='edit_user_by_id'),
+  webapp2.Route('/edit/<user_id:.*>', handler=EditUserHandler, name='edit_user_by_id'),
   webapp2.Route('/competitions/us', handler=BasicHandler('index.html'), name='competitions_us'),
   webapp2.Route('/nationals', handler=BasicHandler('nationals.html'), name='competitions_nationals'),
   webapp2.Route('/regional', handler=BasicHandler('index.html'), name='competitions_regional'),
@@ -38,6 +39,7 @@ app = webapp2.WSGIApplication([
                 handler=ChampionsByYearHandler),
   webapp2.Route('/async/competitions_us/<year:.*>', handler=USCompetitionsHandler),
   # Admin
+  webapp2.Route('/admin/edit_users', handler=EditUsersHandler),
   webapp2.Route('/admin/upload_document', handler=UploadDocumentHandler,
                 name='upload_document'),
   webapp2.Route('/admin/delete_document/<document_id:.*>', handler=DeleteDocumentHandler,
---
+++ src/handlers/admin/edit_users.py
@@ -0,0 +1,19 @@
+from src.handlers.admin.admin_base import AdminBaseHandler
+from src import common
+from src.jinja import JINJA_ENVIRONMENT
+from src.models.user import Roles
+from src.models.user import User
+
+# This handler shows all users so that admins can edit them.
+
+class EditUsersHandler(AdminBaseHandler):
+  def get(self):
+    all_users = User.query().order(User.name).fetch() * 2
+    template = JINJA_ENVIRONMENT.get_template('admin/edit_users.html')
+    self.response.write(template.render({
+        'c': common.Common(self),
+        'users': all_users,
+    }))
+
+  def PermittedRoles(self):
+    return Roles.AdminRoles()
