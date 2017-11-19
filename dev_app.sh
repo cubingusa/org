f673a57285e2bb576f56f4b84ccca580af269be0
Tim Reynolds <timbreynolds@gmail.com>
2017-11-19 18:20:13 -0500

    Store dev_appserver data where it won't be cleaned up.

--- dev_app.sh
+++ dev_app.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+# Store local datastore and blobstore locally, to avoid tmp storage getting
+# cleaned up on reboots.
+mkdir -p .local_storage
+
 echo "Starting dev_appserver.py."
-dev_appserver.py app.yaml admin.yaml
+dev_appserver.py app.yaml admin.yaml \
+  --datastore_path=.local_storage/datastore \
+  --blobstore_path=.local_storage/blobstore
