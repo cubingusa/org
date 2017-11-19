db5081cac7a1bc929a3afebe8908b9ce9bcc34aa
Tim Reynolds <timbreynolds@gmail.com>
2017-11-19 17:58:01 -0500

    Delete old app settings handler.

--- admin.py
+++ admin.py
@@ -2,7 +2,6 @@ import webapp2
 
 from src import config
 from src.handlers.admin.assign_role import AssignRoleHandler
-from src.handlers.admin.set_app_settings import SetAppSettingsHandler
 from src.handlers.admin.post_import_mutations import PostImportMutationsHandler
 from src.handlers.admin.update_championships import UpdateChampionshipsHandler
 from src.handlers.admin.update_states import UpdateStatesHandler
@@ -17,6 +16,5 @@ app = webapp2.WSGIApplication([
   webapp2.Route('/upload_users', handler=UploadUsersHandler, name='upload_users'),
   webapp2.Route('/wca/get_export', handler=GetExportHandler),
   webapp2.Route('/assign_role/<user_id:\d+>/<role:.*>', handler=AssignRoleHandler),
-  webapp2.Route('/set_app_settings/<setting:.*>/<value:.*>', handler=SetAppSettingsHandler),
   webapp2.Route('/app_settings', handler=AppSettingsHandler, name='app_settings'),
 ], config=config.GetAppConfig())
