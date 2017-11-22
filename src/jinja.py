c20e59af1539305ff7d3b379fc3b4c7cf9cd18df
Tim Reynolds <timbreynolds@gmail.com>
2017-11-21 22:06:59 -0500

    Strip whitespace by default.

--- src/jinja.py
+++ src/jinja.py
@@ -4,4 +4,6 @@ import os
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + '/templates'),
     extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
+    autoescape=True,
+    trim_blocks=True,
+    lstrip_blocks=True)
