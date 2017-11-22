a0ffe130f9a992720de3862e18d40b1f2423481a
Tim Reynolds <timbreynolds@gmail.com>
2017-11-21 20:54:10 -0500

    Re-enable login on admin.

--- admin.py
+++ admin.py
@@ -8,8 +8,14 @@ from src.handlers.admin.update_states import UpdateStatesHandler
 from src.handlers.admin.upload_users import UploadUsersHandler
 from src.handlers.admin.get_wca_export import GetExportHandler
 from src.handlers.admin.app_settings import AppSettingsHandler
+from src.handlers.login import LoginHandler
+from src.handlers.login import LoginCallbackHandler
+from src.handlers.login import LogoutHandler
 
 app = webapp2.WSGIApplication([
+  webapp2.Route('/login', handler=LoginHandler, name='login'),
+  webapp2.Route('/login_callback', handler=LoginCallbackHandler, name='login_callback'),
+  webapp2.Route('/logout', handler=LogoutHandler, name='logout'),
   webapp2.Route('/post_import_mutations', handler=PostImportMutationsHandler),
   webapp2.Route('/update_championships', handler=UpdateChampionshipsHandler),
   webapp2.Route('/update_states', handler=UpdateStatesHandler),
