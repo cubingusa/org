959ec7f607b0216effe5ce2ff2594f22b5e0bffa
Tim Reynolds <timbreynolds@gmail.com>
2017-11-23 12:27:51 -0500

    Give feedback on invalid locations.

--- src/static/js/edit_user.js
+++ src/static/js/edit_user.js
@@ -1,7 +1,14 @@
 editUserModule = (function() {
-  var select = document.getElementById('state');
+  var city = document.getElementById('city');
+  var state = document.getElementById('state');
   var region = document.getElementById('region');
+  var lat = document.getElementById('lat');
+  var lng = document.getElementById('lng');
+
   var region_id = region.dataset.regionid;
+
+  var geocodingFailed = document.getElementById('geocoding-failed');
+  var stateMissing = document.getElementById('state-missing');
 
   var allowSubmission = function() {
     document.getElementById('form').submit();
@@ -9,7 +16,7 @@ editUserModule = (function() {
   
   return {
     onStateChange: function() {
-      selected = select.options[select.selectedIndex];
+      selected = state.options[state.selectedIndex];
       region.classList.remove('region-' + region_id);
       region_id = selected.dataset.regionid;
       region.value = selected.dataset.regionname;
@@ -17,8 +24,9 @@ editUserModule = (function() {
     },
 
     validate: function(event) {
-      if (document.getElementById('lat').value ||
-          !document.getElementById('city').value) {
+      geocodingFailed.style.display = 'none';
+      stateMissing.style.display = 'none';
+      if (lat.value || !city.value) {
         // If we've already populated the latitude and longitude, or they didn't provide
         // a city, allow the submission.
         return;
@@ -31,26 +39,23 @@ editUserModule = (function() {
       event.preventDefault();
       event.stopPropagation();
       // If the user provides a city but not a state, require them to fix it.
-      if (!document.getElementById('state').value) {
+      if (state.value === 'empty') {
+        stateMissing.style.display = 'initial';
         return;
       }
-      geocoderModule.geocode(document.getElementById('city').value,
-                             document.getElementById('state').value,
-                             function(geocoding_result) {
-        console.log(geocoding_result);
+      geocoderModule.geocode(city.value, state.value, function(geocoding_result) {
         if (geocoding_result.status !== 'OK') {
+          geocodingFailed.style.display = 'initial';
           return;
         }
         geocoding_result.location.address_components.forEach(function(address_component) {
           address_component.types.forEach(function(type) {
             if (type == 'locality') {
-              document.getElementById('city').value = address_component.long_name;
+              city.value = address_component.long_name;
             }
           });
-          document.getElementById('lat').value =
-              Math.floor(geocoding_result.location.geometry.location.lat() * 1000000);
-          document.getElementById('lng').value =
-              Math.floor(geocoding_result.location.geometry.location.lng() * 1000000);
+          lat.value = Math.floor(geocoding_result.location.geometry.location.lat() * 1000000);
+          lng.value = Math.floor(geocoding_result.location.geometry.location.lng() * 1000000);
           allowSubmission();
           return;
         });
