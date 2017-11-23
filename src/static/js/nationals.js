da70097d2f339977d6b3ee78daf7c66de74d835a
Tim Reynolds <timbreynolds@gmail.com>
2017-11-23 15:37:23 -0500

    Fade in nationals winners.

--- src/static/js/nationals.js
+++ src/static/js/nationals.js
@@ -4,12 +4,14 @@ var nationalsModule = (function() {
       var req = new XMLHttpRequest();
       document.getElementById('nats-spinner').style.display = 'inherit';
       document.getElementById('champions-table').innerHTML = '';
+      document.getElementById('champions-table').classList.remove('fade-in');
       req.onreadystatechange = function() {
         if (req.readyState == 4) {
           document.getElementById('nats-spinner').style.display = 'none';
           if (req.status == 200) {
             document.getElementById('nationals-evt').innerHTML = event_name;
             document.getElementById('champions-table').innerHTML = req.responseText;
+            document.getElementById('champions-table').classList.add('fade-in');
           } else {
             document.getElementById('nationals-evt').innerHTML = '';
           }
