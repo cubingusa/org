52e6f84c81d8dccd9413377f3c6fe126a25441d1
Tim Reynolds <timbreynolds@gmail.com>
2017-12-09 14:43:42 -0500

    Reload the page on change to dates.

--- src/static/js/scheduling/edit_schedule.js
+++ src/static/js/scheduling/edit_schedule.js
@@ -66,11 +66,7 @@ var editScheduleModule = (function() {
                '/' + startDateElement.value +
                '/' + endDateElement.value);
       req.addEventListener('load', function() {
-        document.getElementById('stages-section').style.display = 'inherit';
-        document.getElementById('rounds-section').style.display = 'inherit';
-        if (eventId !== null) {
-          editScheduleModule.selectEvent(eventId, eventName);
-        }
+        location.reload();
       });
       req.send();
     },
