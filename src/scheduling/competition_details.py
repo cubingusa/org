980bbb22cf08b6b3d84886c3731cf1f5ef419b99
Tim Reynolds <timbreynolds@gmail.com>
2017-12-16 14:31:20 -0500

    Initial implementation of event list.

--- src/scheduling/competition_details.py
+++ src/scheduling/competition_details.py
@@ -1,6 +1,104 @@
+import json
+
+from google.appengine.ext import ndb
+
+from src.models.scheduling.round import ScheduleRound
+from src.models.scheduling.schedule import Schedule
+from src.models.scheduling.time_block import ScheduleTimeBlock
+
+
+class GroupedTimeBlock(object):
+  def __init__(self, t):
+    self._time_blocks = [t]
+    self._start_time = t.GetStartTime()
+    self._end_time = t.GetEndTime()
+    self._stage_keys = set([t.stage])
+
+  def AddTimeBlock(self, t):
+    self._time_blocks.append(t)
+    self._end_time = max(self._end_time, t.GetEndTime())
+    self._stage_keys.add(t.stage)
+
+  def GetStartTime(self):
+    return self._start_time
+
+  def GetEndTime(self):
+    return self._end_time
+
+  def FormatTimes(self):
+    return '%s %s - %s' % (
+        self._start_time.strftime('%A'),
+        self._start_time.strftime('%I:%M %p').lstrip('0'),
+        self._end_time.strftime('%I:%M %p').lstrip('0'))
+
+
+class RoundDetails(object):
+  def __init__(self, r):
+    self._r = r
+    self._grouped_time_blocks = []
+
+  def GetRound(self):
+    return self._r
+
+  def AddTimeBlock(self, t):
+    print 'AddTimeBlock'
+    if (self._grouped_time_blocks and
+        t.GetStartTime() < self._grouped_time_blocks[-1].GetEndTime()):
+      self._grouped_time_blocks[-1].AddTimeBlock(t)
+    else:
+      self._grouped_time_blocks.append(GroupedTimeBlock(t))
+
+  def GetGroupedTimeBlocks(self):
+    return self._grouped_time_blocks
+    
+
+class EventDetails(object):
+  def __init__(self, event_key):
+    self._event_key = event_key
+    self._rounds = {}
+
+  def AddRound(self, r):
+    self._rounds[r.number] = RoundDetails(r)
+
+  def AddTimeBlock(self, t):
+    self._rounds[t.round.get().number].AddTimeBlock(t)
+
+  def GetEvent(self):
+    return self._event_key.get()
+
+  def GetRounds(self):
+    return self._rounds.values()
+
+  def GetQualifyingTime(self):
+    return '20.00'
+
+  def QualifyingTimeIsAverage(self):
+    return True
 
 
 class CompetitionDetails(object):
   def __init__(self, user, competition):
     self.user = user
     self.competition = competition
+    self.schedule = Schedule.query(ndb.AND(Schedule.competition == competition.key,
+                                           Schedule.is_live == True)).get()
+    if not self.schedule:
+      return
+    print self.schedule.key.id()
+
+    self.events = {}
+    self.event_keys = []
+    for r in ScheduleRound.query(ScheduleRound.schedule == self.schedule.key).iter():
+      if r.event.id() not in self.events:
+        self.events[r.event.id()] = EventDetails(r.event)
+        self.event_keys.append(r.event)
+      self.events[r.event.id()].AddRound(r)
+
+    for t in (ScheduleTimeBlock.query(ndb.AND(ScheduleTimeBlock.schedule == self.schedule.key,
+                                              ScheduleTimeBlock.staff_only == False))
+                               .order(ScheduleTimeBlock.start_time)
+                               .iter()):
+      self.events[t.round.get().event.id()].AddTimeBlock(t)
+
+  def GetEvents(self):
+    return sorted(self.events.values(), key=lambda e: e.GetEvent().rank)
