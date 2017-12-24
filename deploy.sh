8a93a940f7e22067f5b589ec9d4b4bc3417db757
Tim Reynolds <timbreynolds@gmail.com>
2017-12-24 16:11:18 -0500

    Require a version name for the staging app.

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,33 @@
 #!/usr/bin/env bash
 
-# Usage:
-# ./deploy.sh -p -f app.yaml -- to just deploy the main website to prod.
-# ./deploy.sh -s -f app.yaml -- to deploy to staging.
-# ./deploy.sh -p -f *.yaml -- deploy the entire app to prod.
+# Arguments:
+# -p: deploy to prod
+# -s: deploy to staging
+# -f <file or glob>: .yaml files to deploy
+# -v <app version>: On staging, the name of the app version to upload.
 
 set -e
 
 PROJECT=""
+IS_PROD=0
 FILES_TO_DEPLOY=""
+VERSION=""
 
-while getopts "psf:" opt; do
+while getopts "psf:v:" opt; do
   case $opt in
     p)
       PROJECT="cubingusa-org"
+      IS_PROD=1
       ;;
     s)
       PROJECT="staging-cubingusa-org"
+      IS_PROD=0
       ;;
     f)
       FILES_TO_DEPLOY="$FILES_TO_DEPLOY $OPTARG"
+      ;;
+    v)
+      VERSION="$OPTARG"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -44,15 +52,32 @@ then
   exit 1
 fi
 
+if [ "$IS_PROD" == "1" -a ! -z "$VERSION" ]
+then
+  echo "You can't specify a version for the prod app." >&2
+  exit 1
+fi
+
+if [ "$IS_PROD" == "0" -a -z "$VERSION" ]
+then
+  echo "You must specify a version with -v for the staging app." >&2
+  exit 1
+fi
+
 echo "Updating python dependencies."
-pip2.7 install -t lib -r requirements.txt --upgrade
+#pip2.7 install -t lib -r requirements.txt --upgrade
 
 echo "Recompiling minified CSS."
-rm -r -f src/static/css/prod
-mkdir src/static/css/prod
-sass --update src/scss:src/static/css/prod --style compressed
+#rm -r -f src/static/css/prod
+#mkdir src/static/css/prod
+#sass --update src/scss:src/static/css/prod --style compressed
 
 echo "Deploying to App Engine."
 CMD="gcloud app deploy $FILES_TO_DEPLOY --project $PROJECT"
+if [ ! -z "$VERSION" ]
+then
+  CMD="$CMD --version $VERSION"
+fi
+
 echo "$CMD"
 $CMD
