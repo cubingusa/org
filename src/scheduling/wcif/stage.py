58836a43df36b737390a11520c9e60683629e76f
Tim Reynolds <timbreynolds@gmail.com>
2017-12-25 10:13:42 -0500

    Ensure that venue IDs are integers.

--- src/scheduling/wcif/stage.py
+++ src/scheduling/wcif/stage.py
@@ -1,3 +1,5 @@
+import re
+
 from src.scheduling.wcif.time_block import TimeBlockToWcif
 
 # Writes a ScheduleStage in WCIF format.  The corresponding WCIF entity for a
@@ -5,7 +7,9 @@ from src.scheduling.wcif.time_block import TimeBlockToWcif
 # https://docs.google.com/document/d/1hnzAZizTH0XyGkSYe-PxFL5xpKVWl_cvSdTzlT_kAs8/edit?ts=5a3fd252#heading=h.cllgja7au1th
 def StageToWcif(stage, time_blocks, groups_by_time_block):
   output_dict = {}
-  output_dict['id'] = int(stage.key.id())
+  # Stage ID may be of the form a_b, where a and b are integers.  Use the last
+  # segment to ensure an integer ID.
+  output_dict['id'] = int(re.search('\d*$', stage.key.id()).group(0))
   output_dict['name'] = stage.name
   output_dict['activities'] = [
       TimeBlockToWcif(time_block, groups_by_time_block[time_block.key.id()])
