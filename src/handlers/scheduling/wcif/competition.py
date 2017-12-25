232bf18ddc7f66d6e201fd5c062365adcead2393
Tim Reynolds <timbreynolds@gmail.com>
2017-12-25 11:04:33 -0500

    Add a pretty-print option, and a Content-TYpe.

--- src/handlers/scheduling/wcif/competition.py
+++ src/handlers/scheduling/wcif/competition.py
@@ -13,4 +13,9 @@ class CompetitionWcifHandler(SchedulingBaseHandler):
       return
     schedule = Schedule.query(ndb.AND(Schedule.competition == self.competition.key,
                                       Schedule.is_live == True)).get()
-    self.response.write(json.dumps(CompetitionToWcif(self.competition, schedule)))
+    self.response.headers.add('Content-Type', 'application/json')
+    json_object = CompetitionToWcif(self.competition, schedule)
+    if self.request.get('pretty'):
+      self.response.write(json.dumps(json_object, sort_keys=True, indent=2))
+    else:
+      self.response.write(json.dumps(json_object))
