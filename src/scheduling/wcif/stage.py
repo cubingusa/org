a84ebbdf934711b888fad907ed9ca013d552d380
Tim Reynolds <timbreynolds@gmail.com>
2017-12-26 10:03:08 -0500

    Add ScheduleStage extension.

--- src/scheduling/wcif/stage.py
+++ src/scheduling/wcif/stage.py
@@ -1,5 +1,7 @@
 import re
 
+from src.scheduling.colors import Colors
+from src.scheduling.wcif.extensions import AddExtension
 from src.scheduling.wcif.time_block import TimeBlockToWcif
 
 # Writes a ScheduleStage in WCIF format.  The corresponding WCIF entity for a
@@ -7,11 +9,17 @@ from src.scheduling.wcif.time_block import TimeBlockToWcif
 # https://docs.google.com/document/d/1hnzAZizTH0XyGkSYe-PxFL5xpKVWl_cvSdTzlT_kAs8/edit?ts=5a3fd252#heading=h.cllgja7au1th
 def StageToWcif(stage, time_blocks, groups_by_time_block):
   output_dict = {}
-  # Stage ID may be of the form a_b, where a and b are integers.  Use the last
-  # segment to ensure an integer ID.
-  output_dict['id'] = int(re.search('\d*$', stage.key.id()).group(0))
+  output_dict['id'] = stage.number
   output_dict['name'] = stage.name
   output_dict['activities'] = [
       TimeBlockToWcif(time_block, groups_by_time_block[time_block.key.id()])
       for time_block in time_blocks]
+
+  extension_dict = {}
+  extension_dict['datastoreId'] = stage.key.id()
+  extension_dict['colorCss'] = stage.color
+  extension_dict['colorHex'] = Colors[stage.color]
+  extension_dict['numTimers'] = stage.timers
+  AddExtension('ScheduleStage', extension_dict, output_dict)
+
   return output_dict
