eca11b20c51a17b52bc1f4e8e2fd53577fde1e32
Tim Reynolds <timbreynolds@gmail.com>
2017-12-28 23:47:57 -0500

    Get rid of unneeded print.

--- src/timezones.py
+++ src/timezones.py
@@ -3,7 +3,6 @@ import pytz
 
 # Parses a datetime string to UTC so that it can be stored in a datastore model.
 def ToUTCTime(time_string, time_format, timezone):
-  print time_string, time_format, timezone
   d = datetime.datetime.strptime(time_string, time_format)
   return pytz.timezone(timezone).localize(d).astimezone(pytz.timezone('UTC')).replace(tzinfo=None)
 
