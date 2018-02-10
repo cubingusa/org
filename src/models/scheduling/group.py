a9cad21a3990a63ba6bca38eb78c81defc62a320
Tim Reynolds <timbreynolds@gmail.com>
2018-02-09 22:17:39 -0500

    Add more computed properties to ScheduleGroup.

--- src/models/scheduling/group.py
+++ src/models/scheduling/group.py
@@ -1,5 +1,6 @@
 from google.appengine.ext import ndb
 
+from src import timezones
 from src.models.scheduling.schedule import Schedule
 from src.models.scheduling.time_block import ScheduleTimeBlock
 
@@ -13,3 +14,18 @@ class ScheduleGroup(ndb.Model):
   start_time = ndb.DateTimeProperty()
   end_time = ndb.DateTimeProperty()
   staff_only = ndb.ComputedProperty(lambda self: self.time_block.get().staff_only)
+
+  # Only relevant for events like 333fm and 333mbf.
+  attempt = ndb.ComputedProperty(lambda self: self.time_block.get().attempt)
+
+  def GetStartTime(self):
+    if not self.start_time:
+      return None
+    return timezones.ToLocalizedTime(self.start_time,
+                                     self.schedule.get().competition.get().timezone)
+
+  def GetEndTime(self):
+    if not self.end_time:
+      return None
+    return timezones.ToLocalizedTime(self.end_time,
+                                     self.schedule.get().competition.get().timezone)
