c8bdc3d5532d64e3c60d336847ebf445b94c9306
Tim Reynolds <timbreynolds@gmail.com>
2018-02-16 08:42:34 -0500

    Add an archive service that redirects.

---
+++ archive.py
@@ -0,0 +1,12 @@
+import webapp2
+
+app = webapp2.WSGIApplication([
+  webapp2.Route('/', handler=webapp2.RedirectHandler,
+                defaults={'_uri': 'https://cubingusa.org'}),
+  webapp2.Route('/index.php', handler=webapp2.RedirectHandler,
+                defaults={'_uri': 'https://cubingusa.org'}),
+  webapp2.Route('/faq.php', handler=webapp2.RedirectHandler,
+                defaults={'_uri': 'https://cubingusa.org/about/contact'}),
+  webapp2.Route('/state.php', handler=webapp2.RedirectHandler,
+                defaults={'_uri': 'https://cubingusa.org/state_rankings'}),
+])
