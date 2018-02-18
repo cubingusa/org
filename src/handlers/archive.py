2158b770e147d78abd10ed5bf8197634be818a06
Tim Reynolds <timbreynolds@gmail.com>
2018-02-17 23:32:14 -0500

    Add Content-Type header.

--- src/handlers/archive.py
+++ src/handlers/archive.py
@@ -5,6 +5,18 @@ from src.handlers.base import BaseHandler
 
 class ArchiveHandler(webapp2.RequestHandler):
   def get(self, path):
+    if '.css' in path:
+      self.response.headers['Content-Type'] = 'text/css'
+    elif '.js' in path:
+      self.response.headers['Content-Type'] = 'text/javascript'
+    elif '.php' in path:
+      self.response.headers['Content-Type'] = 'text/html'
+
+    # This file is served at /alerts.css.  It's not really used, so ignore it.
+    if path == 'alerts.css':
+      return
+
+    # Map /comp_id and /comp_id/ to /comp_id/index.php.
     if '/' not in path:
       path = path + '/'
     if path.endswith('/'):
