b6a321e145fc3a607bfe0dde29830a7bbf57440d
Tim Reynolds <timbreynolds@gmail.com>
2018-02-24 22:01:17 -0500

    Work around weirdness with google in python path.

--- appengine_config.py
+++ appengine_config.py
@@ -1,3 +1,12 @@
+import os
+import google
 from google.appengine.ext import vendor
 
-vendor.add('lib')
+lib_directory = os.path.dirname(__file__) + '/lib'
+
+# Change where to find the google package (point to the lib/ directory)
+# Background: https://github.com/GoogleCloudPlatform/google-auth-library-python/issues/169
+google.__path__ = [os.path.join(lib_directory, 'google')] + google.__path__
+
+# Add any libraries install in the "lib" folder.
+vendor.add(lib_directory)
