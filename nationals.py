9c7417c5c37dd82878d0ed68cd8c52b1fd12467c
Tim Reynolds <tim@cubingusa.org>
2018-12-07 07:13:05 -0800

    Initial commit of nats 2019.
    This is mostly copied from 2018.

--- nationals.py
+++ nationals.py
@@ -8,12 +8,12 @@ from src.models.user import Roles
 from src.handlers.nationals.eighteen.events import Events2018Handler
 from src.handlers.nationals.eighteen.groups import Groups2018Handler
 from src.handlers.nationals.eighteen.schedule import Schedule2018Handler
-
-uri_base = '/nationals/2018'
-template_base = 'nationals/2018'
+from src.handlers.nationals.nineteen.events import Events2019Handler
+from src.handlers.nationals.nineteen.schedule import Schedule2019Handler
 
 app = webapp2.WSGIApplication([
   webapp2.Route('/nationals', webapp2.RedirectHandler, defaults={'_uri': '/nationals/2018'}),
+  # 2018
   webapp2.Route('/nationals/2018',
                 handler=BasicHandler('/nationals/2018/index.html',
                                      include_wca_disclaimer=True)),
@@ -34,4 +34,17 @@ app = webapp2.WSGIApplication([
                 handler=Groups2018Handler, name='groups_2018_person'),
   webapp2.Route('/nationals/2018/groups/<event_id:.*>/<round_num:\d>/<stage:.>/<group:\d*>',
                 handler=Groups2018Handler, name='groups_2018_group'),
+  # 2019
+  webapp2.Route('/nationals/2019',
+                handler=BasicHandler('/nationals/2019/index.html',
+                                     include_wca_disclaimer=True)),
+  webapp2.Route('/nationals/2019/events', handler=Events2019Handler,
+                name='events'),
+  webapp2.Route('/nationals/2019/schedule', handler=Schedule2019Handler),
+  webapp2.Route('/nationals/2019/travel',
+                handler=BasicHandler('/nationals/2019/travel.html')),
+  webapp2.Route('/nationals/2019/contact',
+                handler=ContactHandler('nats-organizers@cubingusa.org',
+                                       '/nationals/2019/contact.html',
+                                       'Nationals 2019')),
 ], config=config.GetAppConfig())
