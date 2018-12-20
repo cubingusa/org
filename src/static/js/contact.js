1649cbfd880e2f0e52be8d6b4dc0efadb7fd1d8a
Tim Reynolds <tim@cubingusa.org>
2018-12-20 07:40:44 -0500

    Ask the user to log in if they have a WCA account.

---
+++ src/static/js/contact.js
@@ -0,0 +1,6 @@
+onloadModule.register(function() {
+  document.getElementById('noid').onclick = function() {
+    document.getElementById('form-content-container').style.display = 'block';
+    document.getElementById('noid-container').style.display = 'none';
+  }
+});
