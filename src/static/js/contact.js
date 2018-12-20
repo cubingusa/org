596390ecd3bc38fdea41f74f14b9597ccd4e0a42
Tim Reynolds <tim@cubingusa.org>
2018-12-20 07:55:47 -0500

    Add a field to the contact form for WCA ID.

--- src/static/js/contact.js
+++ src/static/js/contact.js
@@ -1,6 +1,9 @@
 onloadModule.register(function() {
-  document.getElementById('noid').onclick = function() {
-    document.getElementById('form-content-container').style.display = 'block';
-    document.getElementById('noid-container').style.display = 'none';
+  noid = document.getElementById('noid');
+  if (noid) {
+    document.getElementById('noid').onclick = function() {
+      document.getElementById('form-content-container').style.display = 'block';
+      document.getElementById('noid-container').style.display = 'none';
+    }
   }
 });
