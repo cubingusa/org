23bf2bcdd9f7f7df57b124c939cfa200e0715b3f
JP Bulman <jpbulman@wpi.edu>
2019-07-20 21:04:11 -0400

    Revert "Regional champion year seperated from normal year"
    This reverts commit 09dee092b69918e19012801db51a1ea1f0ac3e87.

--- src/handlers/regional.py
+++ src/handlers/regional.py
@@ -1,6 +1,5 @@
 from google.appengine.ext import ndb
 
-import datetime
 from src import common
 from src.handlers.base import BaseHandler
 from src.jinja import JINJA_ENVIRONMENT
@@ -13,7 +12,7 @@ class RegionalsHandler(BaseHandler):
   def get(self):
     # The year we want to display championships for.  We should update this
     # once we're ready to start announcing the next year's championships.
-    year = datetime.datetime.now().year
+    year = 2019
 
     championships = Championship.query(ndb.AND(Championship.year == year,
                                                Championship.region != None)).fetch()
@@ -27,14 +26,10 @@ class RegionalsHandler(BaseHandler):
     regions_missing_championships = [
         region for region in regions if region.key not in championship_regions]
 
-    #If not one of the regionals has happened yet, then show the previous year's champions
-    champion_year = year-1 if len(regions_missing_championships) == 7 else year 
-
     template = JINJA_ENVIRONMENT.get_template('regional.html')
     self.response.write(template.render({
         'c': common.Common(self),
         'year': year,
-        'champion_year':champion_year,
         'championships': championships,
         'regions_missing_championships': regions_missing_championships,
     }))
