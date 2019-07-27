72373fdad55a359bf6eed3d25077908c74c22d36
Tim Reynolds <tim@cubingusa.org>
2019-07-27 19:33:58 -0400

    Redirect /groups/foo to nats 2019.

--- src/handlers/nationals/eighteen/redirect.py
+++ src/handlers/nationals/eighteen/redirect.py
@@ -3,4 +3,4 @@ from src.handlers.base import BaseHandler
 
 class NationalsGroupsRedirectHandler(BaseHandler):
   def get(self, person_id):
-    self.redirect('/nationals/2018/groups/' + person_id)
+    self.redirect('/nationals/2019/groups/' + person_id)
