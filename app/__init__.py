714178dfc7b220edda897edfdb852880b78160b7
Tim Reynolds <tim@cubingusa.org>
2022-02-17 19:42:56 -0500

    Use a custom .env file.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from dotenv import load_dotenv
 from flask import Flask
+import os
+
+env_file = os.environ.get('ENV_PATH') or '.env'
+if not os.path.exists(env_file):
+  raise FileNotFoundError('Could not find environment file ' + env_file)
 
 app = Flask(__name__)
-load_dotenv()
+load_dotenv(os.environ.get('ENV_PATH') or '.env')
 
 from app.cubingusa import bp as cubingusa_bp
 from app.nationals import bp as nationals_bp
