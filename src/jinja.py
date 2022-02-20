bcd9a168c4d22dd99e256034a7bc643420e0a5c0
Tim Reynolds <tim@cubingusa.org>
2022-02-20 08:25:45 -0500

    More cleanup.

--- src/jinja.py
+++
@@ -1,9 +0,0 @@
-import jinja2
-import os
-
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + '/templates'),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True,
-    trim_blocks=True,
-    lstrip_blocks=True)
