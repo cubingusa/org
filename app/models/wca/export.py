698d683adcb6ef9f2c47b9dd73f107b383bca3c9
Tim Reynolds <tim@cubingusa.org>
2022-02-21 08:26:51 -0500

    Fix set_latest_export.

--- app/models/wca/export.py
+++ app/models/wca/export.py
@@ -14,8 +14,9 @@ def get_latest_export():
 
 def set_latest_export(export_id):
   latest = LatestWcaExport.get_by_id('1') or LatestWcaExport(id='1')
-  old_export = None
   if latest.export:
-    old_export = latest.export
+    latest.export.delete()
   latest.export = ndb.Key(WcaExport, export_id)
   latest.put()
+  new_export = WcaExport(id=export_id)
+  new_export.put()
