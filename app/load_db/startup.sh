c20dbd8e50297ab4c86db5f7b5d3d83b4e262fc8
Tim Reynolds <tim@cubingusa.org>
2022-02-24 07:12:44 -0500

    Don't bother with logs, cloud logging is enabled.
    Set CUBINGUSA_ENV from startup.sh.

--- app/load_db/startup.sh
+++ app/load_db/startup.sh
@@ -1,13 +1,9 @@
 # Script run on VM startup.
 # This is a wrapper for load_db.sh; most of the logic should be in there.
+# This is not recommended for running locally, since it sets the environment
+# to COMPUTE_ENGINE.
 
 source env/bin/activate
 pip3 install -r requirements.txt
 
-LOGFILE=/logs/$(date +'%Y-%m-%d-%H:%M:%S')
-
-./app/load_db/load_db.sh 2>&1 >$LOGFILE
-
-# Clean up logs that are older than 30d.
-
-find /logs/* -type f -ctime +30 -exec rm {}
+CUBINGUSA_ENV=COMPUTE_ENGINE ./app/load_db/load_db.sh
