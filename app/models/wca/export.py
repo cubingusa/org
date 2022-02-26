7377b149a066ce82566ac6da07b36482b0e3d44e
Tim Reynolds <tim@cubingusa.org>
2022-02-25 19:42:22 -0500

    Simplify the LatestExport model.

--- app/models/wca/export.py
+++ app/models/wca/export.py
@@ -1,22 +1,15 @@
 from google.cloud import ndb
 
-class WcaExport(ndb.Model):
-  pass
-
 class LatestWcaExport(ndb.Model):
-  export = ndb.KeyProperty(kind=WcaExport)
+  export_id = ndb.StringProperty()
 
 def get_latest_export():
   latest = LatestWcaExport.get_by_id('1')
   if latest:
-    return latest.export.get()
+    return latest.export_id
   return None
 
 def set_latest_export(export_id):
   latest = LatestWcaExport.get_by_id('1') or LatestWcaExport(id='1')
-  if latest.export:
-    latest.export.delete()
-  latest.export = ndb.Key(WcaExport, export_id)
+  latest.export_id = export_id
   latest.put()
-  new_export = WcaExport(id=export_id)
-  new_export.put()
