c0fdfce5e5e542d5c62eb99b3cd06c04b98c48d6
Tim Reynolds <tim@cubingusa.org>
2024-02-23 17:34:08 -0500

    Add url param to the validate handler.

--- app/handlers/admin/validate.py
+++ app/handlers/admin/validate.py
@@ -1,4 +1,4 @@
-from flask import abort, Blueprint
+from flask import abort, Blueprint, request
 import datetime
 from google.cloud import ndb
 
@@ -11,10 +11,14 @@ client = ndb.Client()
 @bp.route('/recent_export')
 def update_states():
   with client.context():
+    if 'days' in request.args:
+      days = int(request.args.get('days'))
+    else:
+      days = 4
     latest = get_latest_export()
     date_part = latest.split('_')[-1][:8]
     ts = datetime.datetime.strptime(date_part, '%Y%m%d')
 
-    if datetime.datetime.now() - ts > datetime.timedelta(days=4):
+    if datetime.datetime.now() - ts > datetime.timedelta(days=days):
       return latest, 500
     return latest, 200
