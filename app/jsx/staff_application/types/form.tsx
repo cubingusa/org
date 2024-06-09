b59a87f6d1f1dd441ec036549c0cea126868ca8d
Tim Reynolds <tim@cubingusa.org>
2024-06-09 13:44:38 -0400

    Initial check-in of questions.

--- app/jsx/staff_application/types/form.tsx
+++ app/jsx/staff_application/types/form.tsx
@@ -1,5 +1,28 @@
 import { DateTime } from "luxon";
 
+export interface YesNoQuestion {
+  questionType: "yes_no";
+}
+
+export enum TextQuestionType {
+  Long = 0,
+  Short = 1,
+}
+
+export interface TextQuestion {
+  questionType: "text";
+  textQuestionType: TextQuestionType;
+}
+
+export interface Question {
+  // Unique within a form.
+  id: number;
+
+  name: string;
+  required: boolean;
+  question: TextQuestion | YesNoQuestion;
+}
+
 export interface Form {
   // Unique within a competition.
   id: number;
