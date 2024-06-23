bf82ac2cb0b5e5904d21aff72cbb26171ea0469c
Tim Reynolds <tim@cubingusa.org>
2024-06-23 08:54:56 -0400

    Define filter api.

---
+++ app/jsx/staff_application/filter/filter.tsx
@@ -0,0 +1,26 @@
+import { Competition } from "@wca/helpers";
+
+import { FilterParams } from "./params";
+import { Trait, TraitComputer } from "../trait/api";
+import { createComputer } from "../trait/create_computer";
+import { ApplicantData } from "../types/applicant_data";
+import { ApplicationSettings } from "../types/competition_data";
+
+export abstract class Filter<T extends Trait> {
+  constructor(
+    private baseParams: FilterParams,
+    settings: ApplicationSettings,
+    wcif: Competition,
+  ) {
+    this.computer = createComputer(baseParams.trait, settings, wcif);
+  }
+
+  apply(applicant: ApplicantData): boolean {
+    const trait = this.computer.compute(applicant) as T;
+    return this.applyImpl(trait);
+  }
+
+  protected abstract applyImpl(val: T): boolean;
+
+  private computer: TraitComputer;
+}
