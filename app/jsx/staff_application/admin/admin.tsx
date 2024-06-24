ab6cadb879e832e127fdddd7643c67aa1201a62c
Tim Reynolds <tim@cubingusa.org>
2024-06-23 20:53:52 -0400

    More resilience on cold start.

--- app/jsx/staff_application/admin/admin.tsx
+++ app/jsx/staff_application/admin/admin.tsx
@@ -29,6 +29,10 @@ export function Admin() {
 
   const newForm = function () {
     event.preventDefault();
+    if (settings.forms === undefined) {
+      settings.forms = [];
+      settings.nextFormId = 0;
+    }
     settings.forms.push({
       id: settings.nextFormId,
       name: "New Form",
@@ -144,7 +148,7 @@ export function Admin() {
         have been accepted to.
       </div>
       <div className="accordion" id="formAccordion">
-        {settings.properties.map((property) => (
+        {(settings.properties || []).map((property) => (
           <div className="accordion-item" key={property.id}>
             <h2 className="accordion-header">
               <button
