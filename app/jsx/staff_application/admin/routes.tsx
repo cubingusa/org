9cda54980363933588c27d0827bbd1cf0de28bbc
Tim Reynolds <tim@cubingusa.org>
2024-06-26 07:00:11 -0400

    Remove old responses page.

--- app/jsx/staff_application/admin/routes.tsx
+++ app/jsx/staff_application/admin/routes.tsx
@@ -1,28 +1,12 @@
 import { Admin } from "./admin";
-import { AdminHeader } from "./header";
-import { Responses } from "./responses/base";
-import { ApplicantLoader } from "./applicant_loader";
-import { CompetitionData } from "../types/competition_data";
 
 export function AdminRoutes() {
   return {
     path: "admin",
-    loader: ApplicantLoader,
     children: [
       {
         index: true,
         element: <Admin />,
-      },
-      {
-        path: "responses",
-        loader: ApplicantLoader,
-        id: "responses",
-        children: [
-          {
-            index: true,
-            element: <Responses />,
-          },
-        ],
       },
     ],
   };
