9f491c571c0cde396ef5999e3725a08ad3274ce0
Tim Reynolds <tim@cubingusa.org>
2024-07-01 21:32:33 -0400

    Dispatch method.

---
+++ app/jsx/staff_application/trait/types/traits.tsx
@@ -0,0 +1,30 @@
+import { Competition } from "@wca/helpers";
+import { TraitTypeApi } from "../api";
+import { TraitType } from "../serialized";
+import { BooleanTraitApi } from "./boolean";
+import { DateTimeTraitApi } from "./date_time";
+import { StringEnumTraitApi, NumberEnumTraitApi } from "./enum";
+import { EventListTraitApi } from "./event_list";
+import { NullTraitApi } from "./null";
+import { NumberTraitApi } from "./number";
+import { StringTraitApi } from "./string";
+
+export function allTraitApis(wcif: Competition): TraitTypeApi[] {
+  return [
+    new BooleanTraitApi(wcif),
+    new DateTimeTraitApi(wcif),
+    new StringEnumTraitApi(wcif),
+    new NumberEnumTraitApi(wcif),
+    new EventListTraitApi(wcif),
+    new NullTraitApi(wcif),
+    new NumberTraitApi(wcif),
+    new StringTraitApi(wcif),
+  ];
+}
+
+export function getApi(
+  type: TraitType,
+  wcif: Competition,
+): TraitTypeApi | null {
+  return allTraitApis(wcif).find((api) => api.type() == type);
+}
