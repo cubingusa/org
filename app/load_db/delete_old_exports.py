d38d6dd2a17cd6e99364edc584b703158cf12b8a
Tim Reynolds <tim@cubingusa.org>
2024-08-12 07:41:21 -0400

    Some more logging in delete_old_exports.

--- app/load_db/delete_old_exports.py
+++ app/load_db/delete_old_exports.py
@@ -16,11 +16,13 @@ client = ndb.Client()
 def main(argv):
   with client.context():
     latest_export = get_latest_export()
+    logging.info('Saving ' + latest_export)
     exports = sorted([f for f in os.listdir(FLAGS.export_base)
                       if not os.path.isfile(os.path.join(FLAGS.export_base, f))
                       and f != latest_export])
 
     for export in exports[:-5]:
+      logging.info('Deleting ' + export)
       shutil.rmtree(os.path.join(FLAGS.export_base, export))
 
 if __name__ == '__main__':
