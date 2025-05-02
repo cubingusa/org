c13e30b455af55996c6f8b4f896e5293961d8c39
Tim Reynolds <tim@cubingusa.org>
2025-05-01 22:19:52 -0400

    Fix loading script.

--- app/load_db/load_nats2025_wcif.py
+++ app/load_db/load_nats2025_wcif.py
@@ -1,15 +1,28 @@
 from google.cloud import ndb
+from google.cloud import storage
+
+import base64
+import json
+import requests
+from requests.auth import HTTPBasicAuth
 
 from app.lib.secrets import get_secret
-from requests.auth import HTTPBasicAuth
 
 client = ndb.Client()
 
 with client.context():
-  username = get_secret('WCA_USERNAME')
-  password = get_secret('WCA_PASSWORD')
+  username = get_secret('WCA_USERNAME').encode('utf-8')
+  password = get_secret('WCA_PASSWORD').encode('utf-8')
+
   auth = HTTPBasicAuth(username, password)
-  wcif = requests.get('https://www.worldcubeassociation.org/api/v0/competitions/CubingUSAAllStars2025/wcif', auth=basic).json()
+  token = requests.post('https://www.worldcubeassociation.org/oauth/token',
+                        {'grant_type': 'password',
+                         'client_id': get_secret('WCA_STAFF_APPLICATION_CLIENT_ID'),
+                         'client_secret': get_secret('WCA_STAFF_APPLICATION_CLIENT_SECRET'),
+                         'username': username,
+                         'password': password,
+                         'scope': 'public manage_competitions'}).json()
+  wcif = requests.get('https://www.worldcubeassociation.org/api/v0/competitions/CubingUSAAllStars2025/wcif', headers=$
   storage_client = storage.Client()
   bucket = storage_client.lookup_bucket('nats2025')
   blob = bucket.get_blob('wcif.json')
