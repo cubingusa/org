cde860c3e144f2ddadfa2524c429350c4601e091
Tim Reynolds <tim@cubingusa.org>
2025-06-18 22:10:41 -0400

    Fixes to load stream.

--- app/load_db/load_wc2025_stream_people.py
+++ app/load_db/load_wc2025_stream_people.py
@@ -1,26 +1,21 @@
 from google.cloud import ndb
 from google.cloud import storage
-from googleapiclient.discovery import build
 
-import base64
-import json
+import os
 import requests
-from requests.auth import HTTPBasicAuth
-
-from app.lib.secrets import get_secret
 
 client = ndb.Client()
 
 with client.context():
   wca_host = os.environ.get('WCA_HOST')
-  data = requests.get'https://api.worldcubeassociation.org/competitions/WC2025/wcif/public')
+  data = requests.get('https://api.worldcubeassociation.org/competitions/WC2025/wcif/public')
   competition = data.json()
   id_to_persons = {}
   out = ''
   for person in competition['persons']:
     id_to_persons[person['wcaUserId']] = person
   for room in competition['schedule']['venues'][0]['rooms']:
-    if room['name'] != 'Main Hall':
+    if room['name'] != 'Hall 4':
       continue
     for activity in room['activities']:
       for child in activity['childActivities']:
@@ -29,9 +24,9 @@ with client.context():
             if ext['id'] == 'groupifier.ActivityConfig' and 'featuredCompetitorWcaUserIds' in ext['data']:
               for user_id in ext['data']['featuredCompetitorWcaUserIds']:
                 person = id_to_persons[user_id]
-                out += '%s,%s,%s\n' % (activity['activityCode'], person['name'], person['wcaId'])
+                out += '%s,%s,%s\n' % (child['activityCode'], person['name'], person['wcaId'])
 
   storage_client = storage.Client()
   bucket = storage_client.lookup_bucket('wc2025')
-  blob = bucket.get_blob('featured.csv')
+  blob = storage.Blob('featured.csv', bucket)
   blob.upload_from_string(out)
